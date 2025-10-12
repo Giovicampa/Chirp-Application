a7fd6127b2794906a2c3923e30b3b84512ef6916
Giovanni Campagna <campagna275@gmail.com>
2025-10-12 14:19:16 +0200

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+	id("chirp.spring-boot-app")
+}
+
+group = "com.plcoding"
+version = "0.0.1-SNAPSHOT"
+description = "Chirp Backend"
+
+dependencies {
+	implementation(projects.user)
+	implementation(projects.chat)
+	implementation(projects.notification)
+	implementation(projects.common)
+	implementation(libs.spring.boot.starter.data.jpa)
+	runtimeOnly(libs.postgresql)
+}
