94e1331a5baeb84c63c4f11c82f8dde083a75585
Giovanni Campagna <campagna275@gmail.com>
2025-10-13 16:18:12 +0200

    Foundation & Setup

--- app/src/main/kotlin/com/plcoding/chirp/ChirpApplication.kt
+++ app/src/main/kotlin/com/plcoding/chirp/ChirpApplication.kt
@@ -1,11 +1,30 @@
 package com.plcoding.chirp
 
+import com.plcoding.chirp.infra.database.entities.UserEntity
+import com.plcoding.chirp.infra.database.repositories.UserRepository
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class ChirpApplication
 
 fun main(args: Array<String>) {
 	runApplication<ChirpApplication>(*args)
+}
+
+@Component
+class Demo(private val repository: UserRepository){
+    
+    @PostConstruct
+    fun init(){
+        repository.save(
+            UserEntity(
+                email = "test@test.com",
+                username = "test",
+                hashedPassword = "123"
+            )
+        )
+    }
 }
