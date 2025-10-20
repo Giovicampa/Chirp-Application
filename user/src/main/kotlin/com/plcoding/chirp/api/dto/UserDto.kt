b9e714d42749f0fad53b1d1656e856fa1a7a38e9
Giovicampa <campagna275@gmail.com>
2025-10-20 18:23:44 +0200

    User Registration

---
+++ user/src/main/kotlin/com/plcoding/chirp/api/dto/UserDto.kt
@@ -0,0 +1,5 @@
+package com.plcoding.chirp.api.dto
+
+import com.plcoding.chirp.domain.model.UserId
+
+data class UserDto(val id: UserId, val email: String, val username: String, val hasVerifiedEmail: Boolean)
